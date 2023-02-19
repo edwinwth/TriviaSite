bf4cd77dfa92a6447acbcbc6c2f7fd8af2eb01fb
Edwin Wong <edwin50093@gmail.com>
2023-02-19 00:38:43 -0500

    Updated TriviaQuestion Serializer

--- backend/trivia/serializers.py
+++ backend/trivia/serializers.py
@@ -10,8 +10,9 @@ class TriviaCategorySerializer(serializers.ModelSerializer):
 
 class TriviaQuestionSerializer(serializers.ModelSerializer):
     created_by = serializers.CharField(source='created_by.username')
+    created_by_id = serializers.IntegerField(source='created_by.id')
     class Meta:
         model = TriviaQuestion
         fields = ['id', 'question_text', 'answer_text', 'trivia_category', 'thumbs_up_count', 'thumbs_down_count',
-                  'creation_date', 'created_by']
+                  'creation_date', 'created_by', 'created_by_id']
         read_only_fields = ['id', 'thumbs_up_count', 'thumbs_down_count', 'creation_date', 'created_by']
